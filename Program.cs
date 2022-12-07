7c887c7cffd61b31b714e7f945ed2e1d8fba341d
Uuljan <uly-9292@mail.ru>
2022-12-07 23:32:39 +0700

    third massage

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if(num1 == quad)
+{
+    Console.WriteLine("yes!");
+}
+else
+{
+    Console.WriteLine("no!");
+}
+*/
+// домашная задания 
+// задача1 
+
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool max = num1 < num2;
+if(num1 < num2)
+{
+    Console.WriteLine("yes!");
+}
+else
+{
+    Console.WriteLine("no!");
+}
+*/
+
+// задача 2
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max=num1; 
+{
+    if(num2 > num1)
+    num2=max;
+    if(num3 > max)
+    max = num3;
+}
+{
+    Console.WriteLine($"max num {max}");
+}
+*/
+
+// задача 3
+/*
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+*/
+
+// задача 4
+/*
+Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int size = 2;
+while (true) 
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i + " ");
+        size++;
+    }
+    if(i >= n)
+    {
+        break;
+    }
+
+    i++;
+}
+*/
+
